90a03d2659b2ebf4468c31cce1e0bb3a5dbb64b5
kak suli <mpsk7492@gmail.com>
2017-11-13 11:32:29 +0330

    Add factorial to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout << "Hi";
+	int n;
+	cin >> n;
+	
+	int ans = 1;
+	for(int i = 1;i <= n;i++){
+		ans *= i;
+	}
+	
+	cout << ans;
 	
 	return 0;
 }
